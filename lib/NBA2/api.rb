51e12277c25e72ab72059997aec9f42f4baa0415
Dmg5th <Dmgould310@gmail.com>
2020-08-20 09:36:12 -0400

    added back height in meters attribute

--- lib/NBA2/api.rb
+++ lib/NBA2/api.rb
@@ -34,7 +34,7 @@ class Api
         
         hash = JSON.parse response.read_body()
         hash["api"]["players"].each do |player| 
-        Player.new(player["firstName"], player["lastName"], player["playerId"], player["country"], player["yearsPro"], player["dateOfBirth"])  
+        Player.new(player["firstName"], player["lastName"], player["playerId"], player["country"], player["heightInMeters"], player["dateOfBirth"])  
         end 
         # binding.pry               
        
--- lib/NBA2/player.rb
+++ lib/NBA2/player.rb
@@ -1,16 +1,16 @@
 class Player 
 
-    attr_accessor :name, :last_name, :player_id, :origin, :years_pro, :dob 
+    attr_accessor :name, :last_name, :player_id, :origin, :height, :dob 
 
     @@all = []
     # binding.pry 
     
-    def initialize(name, last_name, player_id, origin, years_pro, dob)
+    def initialize(name, last_name, player_id, origin, height, dob)
         @name = name 
         @last_name = last_name 
         @player_id = player_id 
         @origin = origin 
-        @years_pro = years_pro
+        @height = height 
         @dob = dob 
         @@all << self 
     end 
@@ -21,22 +21,18 @@ class Player
 
     def print_info
         if @origin.empty?
-            puts "Origin unknown, #{@name} #{last_name} has been in the league for #{years_pro} years, and was born #{@dob}"
+            puts "Origin unknown, #{@name} #{last_name} is #{@height} meters tall, and was born #{@dob}"
        
-        elsif @years_pro == "0"
-            puts "#{@name} #{last_name}'s country origin is #{@origin}, and was born #{@dob}"
-
-        elsif @dob.empty? 
-            puts "#{@name} #{last_name}'s country origin is #{@origin} and has been in the league for #{years_pro} years,"
+        elsif @height.empty?
+            puts "#{@name} #{last_name} is from #{@origin}, and was born #{@dob}"
       
-        elsif @dob.empty? && @origin.empty?
-            puts "#{@name} #{last_name} has been in the league for #{years_pro} years,"
+        elsif @dob.empty?
+            puts "#{@name} #{last_name} is from #{@origin} and is #{@height} meters tall."
         
-       elsif @origin.empty? && @dob.empty? && @years_pro == 0
-            puts "There is currenlty no additional information for #{@name} #{last_name} "
         else 
-        puts "#{@name} #{last_name}'s country origin is #{@origin}, was born #{@dob} and has been in the league for #{years_pro} years,"
-      end 
+        puts "#{@name} #{last_name} is from #{@origin} and is #{@height} meters tall, and was born #{@dob}"
+    
+        end 
     end 
     
 
